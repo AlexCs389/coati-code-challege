8d646603250b58283562c5a2ee9f6509ab6999b9
Alejandro <alejandro.cs389@gmail.com>
2023-04-17 18:26:56 -0500

    Se agrego logica para guardar los datos en la base de datos y redirecciones correspondientes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,12 @@ class HomeController < ApplicationController
   def add_data
     file_path = params[:file].blank? ? nil : params[:file].path
     result = User::ImportData.run(file_path: file_path)
-    p result
+    
+    if result.valid?
+      redirect_to :home_index
+    else
+      redirect_to :import_new_data_home_index
+    end
   end
 
   def import_new_data
--- app/interactions/user/import_data.rb
+++ app/interactions/user/import_data.rb
@@ -3,7 +3,24 @@ class User::ImportData < ActiveInteraction::Base
 
     def execute
         TSV[file_path].map do |row|
-          p row['Cliente']
+          sale = Sale.new(amount_of_pieces: row['Numero de piezas'])
+
+          sale.client = Client.find_or_initialize_by(name: row['Cliente'])
+
+          sale.product = Product.find_or_initialize_by(
+            description: row['Descripción del Producto'],
+            price_per_piece: row['Precio por pieza']
+          )
+
+          sale.seller = Seller.find_or_initialize_by(
+            name: row['Nombre del Vendedor'],
+            address: row['Diección del vendedor']
+          )
+
+          unless sale.save
+            errors.merge!(sale.errors)
+            break
+          end
         end
     end
   end
